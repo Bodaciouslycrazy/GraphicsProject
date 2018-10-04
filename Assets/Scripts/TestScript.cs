526b6b935c45c970f22ab388428b20e661a134db
Bodie Malik <bodiemalik@gmail.com>
2018-10-03 19:44:15 -0500

    initial commit

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+	public Vector3 Rotation;
+	public float Speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate(Rotation * Time.deltaTime * Speed, Space.Self);
+
+	}
+}
