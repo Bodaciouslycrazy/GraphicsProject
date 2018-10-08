92687c39a7fa8086d45ad1a3365e76569e66991b
Bodaciouslycrazy <bodiemalik@gmail.com>
2018-10-08 17:45:19 -0500

    better camera and plane rotation

---
+++ Assets/Scripts/CamreaFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamreaFollow : MonoBehaviour {
+
+	public Transform Following;
+
+	public float PositionLerp = .5f;
+	public float RotationLerp = .5f;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, Following.position, PositionLerp);
+		transform.rotation = Quaternion.Lerp(transform.rotation, Following.rotation, RotationLerp);
+	}
+}
--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -6,7 +6,9 @@ public class Rotator : MonoBehaviour {
 
 	public Rigidbody rb;
 	public float rollSpeed = 10f;
+	public float rollAccel = 100f;
 	public float pitchSpeed = 6f;
+	public float pitchAccel = 100f;
 	public float thrust = 100f;
 
 	// Use this for initialization
@@ -21,16 +23,29 @@ public class Rotator : MonoBehaviour {
 
 	private void FixedUpdate()
 	{
-		Vector3 vel = new Vector3();
+		Vector3 targVect = new Vector3();
 
-		vel.z = -1 * Input.GetAxisRaw("Horizontal") * rollSpeed;
-		vel.x = Input.GetAxisRaw("Vertical") * pitchSpeed;
+		targVect.z = -1 * Input.GetAxisRaw("Horizontal") * rollSpeed;
+		targVect.x = Input.GetAxisRaw("Vertical") * pitchSpeed;
 
+		Vector3 velDiff = targVect - transform.InverseTransformVector(rb.angularVelocity);
 
-		rb.angularVelocity = transform.TransformVector(vel);
+		Vector3 accel = velDiff / Time.fixedDeltaTime;
+		if (accel.z > rollAccel)
+			accel.z = rollAccel;
+		else if (accel.z < -rollAccel)
+			accel.z = -rollAccel;
 
+		if (accel.x > pitchAccel)
+			accel.x = pitchAccel;
+		else if (accel.x < -pitchAccel)
+			accel.x = -pitchAccel;
+
+		rb.AddTorque(transform.TransformVector(accel * rb.mass));
+		//rb.angularVelocity = transform.TransformVector(vel);
+
+		//Needs to be removed
 		float thr = Input.GetButton("Fire1") ? thrust : 0;
 		rb.AddForce(transform.forward * thr);
-
 	}
 }
